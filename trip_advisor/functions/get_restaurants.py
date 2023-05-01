dc853a0b4e1449a928ab6aa949fd7cfedbb70361
KristiyanBulga <56537891+KristiyanBulga@users.noreply.github.com>
2023-05-01 12:17:00 +0200

    Lambda with capture for testing and solving errors

--- trip_advisor/functions/get_restaurants.py
+++ trip_advisor/functions/get_restaurants.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from utils.helper import store_in_s3_bucket, CHROMIUM_PATH, CHROMEDRIVER_PATH
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 import boto3
 
@@ -26,29 +27,58 @@ def handler(event, context) -> None:
     """
     options = Options()
     options.binary_location = CHROMIUM_PATH
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--single-process')
+    options.add_argument('--autoplay-policy=user-gesture-required')
+    options.add_argument('--disable-background-networking')
+    options.add_argument('--disable-background-timer-throttling')
+    options.add_argument('--disable-backgrounding-occluded-windows')
+    options.add_argument('--disable-breakpad')
+    options.add_argument('--disable-client-side-phishing-detection')
+    options.add_argument('--disable-component-update')
+    options.add_argument('--disable-default-apps')
     options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-gpu-sandbox')
+    options.add_argument('--disable-domain-reliability')
+    options.add_argument('--disable-extensions')
+    options.add_argument('--disable-features=AudioServiceOutOfProcess')
+    options.add_argument('--disable-hang-monitor')
+    options.add_argument('--disable-ipc-flooding-protection')
+    options.add_argument('--disable-notifications')
+    options.add_argument('--disable-offer-store-unmasked-wallet-cards')
+    options.add_argument('--disable-popup-blocking')
+    options.add_argument('--disable-print-preview')
+    options.add_argument('--disable-prompt-on-repost')
+    options.add_argument('--disable-renderer-backgrounding')
+    options.add_argument('--disable-setuid-sandbox')
+    options.add_argument('--disable-speech-api')
+    options.add_argument('--disable-sync')
+    options.add_argument('--disk-cache-size=33554432')
+    options.add_argument('--hide-scrollbars')
+    options.add_argument('--ignore-gpu-blacklist')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--metrics-recording-only')
+    options.add_argument('--mute-audio')
+    options.add_argument('--no-default-browser-check')
+    options.add_argument('--no-first-run')
+    options.add_argument('--no-pings')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--no-zygote')
+    options.add_argument('--password-store=basic')
+    options.add_argument('--use-gl=swiftshader')
+    options.add_argument('--use-mock-keychain')
+    options.add_argument('--single-process')
+    options.add_argument('--headless')
     options.add_argument('--window-size=1920x1080')
-    options.add_argument(
-        '"user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"')
 
-    # options.add_argument("--headless")  # Hide the GUI
-    # options.add_argument("--no-sandbox")  # No protection needed
-    # options.add_argument("--window-size=1920x1080")  # Setup a fixed screens size
-    # options.add_argument("--single-process")  # Lambda only give us only one CPU
-    # options.add_argument("--no-zygote")  # Don't create zygote processes because Lambda give us only one CPU
-    # options.add_argument("--disable-dev-shm-usage")  # Create temporary folder for shared memory files
-    # options.add_argument("--disable-dev-tools")  # Disable Chrome dev tools
-    # options.add_argument(f"--user-data-dir={mkdtemp()}")  # Create temporary folder to user data
-    # options.add_argument(f"--data-path={mkdtemp()}")  # Create temporary folder to browser data
-    # options.add_argument(f"--disk-cache-dir={mkdtemp()}")  # Create temporary folder to cache
-    # options.add_argument("--remote-debugging-port=9222")
-    # options.add_argument("--enable-javascript")
-    # options.add_argument("--disable-blink-features=AutomationControlled")
-    driver = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=options)
+    options.add_argument('--user-data-dir={}'.format('/tmp/user-data'))
+    options.add_argument('--data-path={}'.format('/tmp/data-path'))
+    options.add_argument('--homedir={}'.format('/tmp'))
+    options.add_argument('--disk-cache-dir={}'.format('/tmp/cache-dir'))
+
+    caps = DesiredCapabilities().CHROME
+    # caps["pageLoadStrategy"] = "normal"  # complete
+    caps["pageLoadStrategy"] = "eager"  # interactive
+    # caps["pageLoadStrategy"] = "none"
+
+    driver = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=options, desired_capabilities=caps)
 
     today = datetime.today()
     tomorrow = today + timedelta(days=1)
@@ -106,7 +136,7 @@ def handler(event, context) -> None:
 
     # push search button
     driver.find_element(By.ID, 'RESTAURANT_SEARCH_BTN').click()
-    time.sleep(45)
+    time.sleep(10)
 
     path_file = f'/tmp/{image_name}_03_after_button.png'
     driver.save_screenshot(path_file)
@@ -116,8 +146,7 @@ def handler(event, context) -> None:
     restaurants_per_page = 30
     # number of found restaurants
     component_36 = driver.find_element(By.ID, 'component_36')
-    # num_restaurants = component_36.find_element(By.CSS_SELECTOR, '.b').get_attribute("innerHTML")
-    num_restaurants = 354
+    num_restaurants = component_36.find_element(By.CSS_SELECTOR, '.b').get_attribute("innerHTML")
     logging.info(f"{num_restaurants} restaurants have been found")
     links = []
 
@@ -135,7 +164,7 @@ def handler(event, context) -> None:
         pages_links = driver.find_element(By.CSS_SELECTOR, '.unified.pagination.js_pageLinks')
         if i < int(num_restaurants)//restaurants_per_page:
             pages_links.find_element(By.XPATH, "//a[normalize-space()="+str(i+2)+"]").click()
-            time.sleep(12)
+            time.sleep(10)
             path_file = f'/tmp/{image_name}_04_page_{i+2}.png'
             driver.save_screenshot(path_file)
             s3_client.upload_file(path_file, ta_bucket, f"testing/{image_name}_04_page_{i+2}.png")
@@ -145,7 +174,7 @@ def handler(event, context) -> None:
     logging.info("Obtained all links. Storing file to S3")
 
     # Write in a file all the data
-    s3_path = f"raw_data/links/{today.strftime('%Y/%m/%d')}"
     filename = f"ta_restaurants_links_{today.strftime('%Y%m%d%H%M%S')}"
+    s3_path = f"raw_data/links/{today.strftime('%Y/%m/%d')}"
     store_in_s3_bucket(ta_bucket, s3_path, links, filename)
     logging.info("Process finished")
--- trip_advisor/utils/helper.py
+++ trip_advisor/utils/helper.py
@@ -1,25 +1,31 @@
 import boto3
+import logging
 import json
 import os
 
 region = "eu-west-1"
 CHROMEDRIVER_PATH = "/opt/chromedriver"
-CHROMIUM_PATH = "/opt/chrome/chrome"
+CHROMIUM_PATH = "/opt/headless-chromium"
 
 
 def _data_to_file(data, filename, extension="json"):
-    path = f"/tmp/{filename}."
+    path = "/tmp/"
+    filename_w_extension = filename
     if extension == "json":
-        path += "json"
+        filename_w_extension += ".json"
+        path += filename_w_extension
         with open(path, 'w', encoding='utf-8') as f:
             json.dump({"restaurants": data}, f, ensure_ascii=False)
             f.close()
-        return path
-    return None
+        return path, filename_w_extension
+    return None, filename
 
 
 def store_in_s3_bucket(bucket, s3_path, data, filename):
     s3_client = boto3.client('s3', region_name=region)
-    path_file = _data_to_file(data, filename)
-    s3_client.upload_file(path_file, bucket, s3_path)
+    path_file, filename_w_extension = _data_to_file(data, filename)
+    if path_file is None:
+        logging.error("File extension selected is not available. Aborting saving")
+        return
+    s3_client.upload_file(path_file, bucket, f"{s3_path}/{filename_w_extension}")
     os.remove(path_file)
