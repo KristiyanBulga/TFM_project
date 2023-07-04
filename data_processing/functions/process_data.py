1241df28da42b855dd4292bbcae68cf7b192386b
KristiyanBulga <56537891+KristiyanBulga@users.noreply.github.com>
2023-07-04 19:50:15 +0200

    Processing google maps data

--- data_processing/functions/process_data.py
+++ data_processing/functions/process_data.py
@@ -10,22 +10,20 @@ logging.getLogger().setLevel(logging.INFO)
 ta_day_conversion = {"lun": "lunes", "mar": "martes", "mié": "miércoles", "jue": "jueves", "vie": "viernes",
                      "sáb": "sábado", "dom": "domingo"}
 
-
-def _data_process_trip_advisor(restaurant_data: dict, ta_place_id: str, ta_restaurant_id: str):
+def _data_process_trip_advisor(restaurant_data: dict, ta_place_id: str, ta_restaurant_id: str, today: datetime):
     logging.info(f"Restaurant {ta_place_id}-{ta_restaurant_id}: processing data from Trip Advisor")
     restaurant_k = restaurant_data["ta_restaurant"]
     data = restaurant_k["data"]
     prices = data["price"] if data.get("price") is not None else {}
     restaurant_info = {
-        "ta_place_id": ta_place_id,
         "ta_restaurant_id": ta_restaurant_id,
+        "added_ts": int(today.timestamp() * 100),
         "name": unescape(restaurant_k.get("name", "")),
         "url": restaurant_k.get("link", ""),
         "symbol": json.dumps([s.count('€') for s in data["symbol"].split("-")] if data.get("symbol") is not None else []),
         "claimed": data.get("claimed", False),
         "price_lower": prices.get("lower"),
         "price_upper": prices.get("upper"),
-        "schedule": dict(),
         "score_overall": data.get("score_overall"),
         "score_food": data.get("score_food"),
         "score_service": data.get("score_service"),
@@ -39,8 +37,7 @@ def _data_process_trip_advisor(restaurant_data: dict, ta_place_id: str, ta_resta
         "serves_breakfast": "Desayuno" in data["meals"] if data.get("meals") is not None else False,
         "serves_brunch": "Brunch" in data["meals"] if data.get("meals") is not None else False,
         "serves_lunch": "Comidas" in data["meals"] if data.get("meals") is not None else False,
-        "serves_dinner": "Cenas" in data["meals"] if data.get("meals") is not None else False,
-        "tags": dict()
+        "serves_dinner": "Cenas" in data["meals"] if data.get("meals") is not None else False
     }
     if restaurant_info["price_lower"] is not None and restaurant_info["price_lower"] is not None:
         restaurant_info["price_mean"] = restaurant_info["price_upper"] - restaurant_info["price_lower"]
@@ -50,15 +47,17 @@ def _data_process_trip_advisor(restaurant_data: dict, ta_place_id: str, ta_resta
         restaurant_info["price_mean"] = restaurant_info["price_upper"]
     else:
         restaurant_info["price_mean"] = None
+    schedule = dict()
     days = data["schedule"] if data.get("schedule") else {}
     for day in days.keys():
-        restaurant_info["schedule"][ta_day_conversion[day]] = data["schedule"][day]
-    restaurant_info["schedule"] = json.dumps(restaurant_info["schedule"])
-    restaurant_info["tags"]["type"] = data["type"] if data.get("type") is not None else []
-    restaurant_info["tags"]["special_diets"] = data["special_diets"] if data.get("special_diets") is not None else []
-    restaurant_info["tags"]["meals"] = data["meals"] if data.get("meals") is not None else []
-    restaurant_info["tags"]["advantages"] = data["advantages"] if data.get("advantages") is not None else []
-    restaurant_info["tags"] = json.dumps(restaurant_info["tags"])
+        schedule[ta_day_conversion[day]] = data["schedule"][day]
+    restaurant_info["schedule"] = json.dumps(schedule)
+    tags = dict()
+    tags["type"] = data["type"] if data.get("type") is not None else []
+    tags["special_diets"] = data["special_diets"] if data.get("special_diets") is not None else []
+    tags["meals"] = data["meals"] if data.get("meals") is not None else []
+    tags["advantages"] = data["advantages"] if data.get("advantages") is not None else []
+    restaurant_info["tags"] = json.dumps(tags)
 
     # Store in a file the reviews
     reviews = data.get("reviews", [])
@@ -81,8 +80,66 @@ def _data_process_trip_advisor(restaurant_data: dict, ta_place_id: str, ta_resta
     return restaurant_info
 
 
-def _data_process_google_maps(restaurant_data):
-    pass
+def _data_process_google_maps(restaurant_data: dict, ta_place_id: str, ta_restaurant_id: str, today: datetime):
+    logging.info(f"Restaurant {ta_place_id}-{ta_restaurant_id}: processing data from Google Chrome")
+    data = restaurant_data["result"]
+    restaurant_info = {
+        "ta_restaurant_id": ta_restaurant_id,
+        "gm_place_id": data.get("place_id"),
+        "added_ts": int(today.timestamp() * 100),
+        "name": data["name"],
+        "url": data.get("url", ""),
+        "symbol": data.get("price_level"),
+        "score_overall": data.get("rating"),
+        "address": data.get("formatted_address"),
+        "webpage": data.get("website"),
+        "phone": data.get("international_phone_number"),
+        "business_status": data.get("business_status", "OPERATIONAL"),
+        "serves_lunch": data.get("serves_lunch", False),
+        "serves_dinner": data.get("serves_dinner", False),
+        "serves_beer": data.get("serves_beer", False),
+        "serves_vegetarian_food": data.get("serves_vegetarian_food", False),
+        "serves_wine": data.get("serves_wine", False),
+        "takeout": data.get("takeout", False),
+        "wheelchair_accessible_entrance": data.get("wheelchair_accessible_entrance", False),
+        "dine_in": data.get("dine_in", False),
+        "delivery": data.get("delivery", False),
+        "reservable": data.get("delivery", False),
+    }
+
+    schedule = dict()
+    for working_day in data.get("opening_hours", {}).get("weekday_text", []):
+        day, info = working_day.split(':', 1)
+        list_hours = []
+        for hour_info in info.split('–'):
+            hour_list = hour_info.split(' ')
+            if len(hour_list) == 2 and hour_list[1] == "PM":
+                hour, mi = hour_list[0].split(':')
+                hour = int(hour) + 12
+                list_hours.append(f"{hour}:{mi}")
+            else:
+                list_hours.append(hour_list[0].strip())
+        schedule[day] = list_hours
+    restaurant_info["schedule"] = json.dumps(schedule)
+
+    # Store in a file the reviews
+    reviews = data.get("reviews", [])
+    logging.info(f"Found {len(reviews)} reviews for {ta_place_id}-{ta_restaurant_id}")
+    for review in reviews:
+        timestamp = review["time"]
+        key = {
+            'place': {'S': f"{ta_place_id}-{ta_restaurant_id}"},
+            'timestamp': {'N': str(int(timestamp * 1000))}
+        }
+        upd_expr = 'SET rate = :rvw_rate, review = :rvw_text, platform =:rvw_platform'
+        expression_attr = {
+            ':rvw_rate': {'N': str(review["rating"])},
+            ':rvw_text': {'S': review["text"]},
+            ':rvw_platform': {'S': "trip_advisor"}
+        }
+        update_item_dynamo(comments_db, key, upd_expr, expression_attr)
+    return restaurant_info
+
 
 def handler(event, context) -> None:
     """
@@ -94,8 +151,10 @@ def handler(event, context) -> None:
     if ta_place_id is None or platform is None:
         logging.error(f"Missing data in the event. Expected keys: trip_advisor_place_id and platform. Event: {event}")
         return
-    # today = datetime.today()
-    today = datetime(2023, 6, 17, 20, 0)
+    if event.get("custom_date", None) is not None:
+        today = datetime.strptime(event["custom_date"], "%Y_%m_%d_%H_%M_%S")
+    else:
+        today = datetime.today()
     today_iso = today.isocalendar()
 
     # Get all the valid restaurants
@@ -128,10 +187,10 @@ def handler(event, context) -> None:
             contents = data['Body'].read().decode("utf-8")
             restaurant_data = json.loads(contents)
             if platform == "trip_advisor":
-                restaurants_data.append(_data_process_trip_advisor(restaurant_data, ta_place_id, ta_restaurant_id))
+                restaurants_data.append(_data_process_trip_advisor(restaurant_data, ta_place_id, ta_restaurant_id, today))
             elif platform == "google_maps":
-                restaurants_data.append(_data_process_google_maps(restaurant_data))
+                restaurants_data.append(_data_process_google_maps(restaurant_data, ta_place_id, ta_restaurant_id, today))
 
     filename = f"{platform}_{today.strftime('%Y_%m_%d_%H_%M_%S')}"
-    s3_path = f"restaurants/data/place={ta_place_id}/year={today_iso.year}/week={today_iso.week}"
+    s3_path = f"restaurants/data/ta_place_id={ta_place_id}/year={today_iso.year}/week={today_iso.week}"
     store_in_s3_bucket(bucket, s3_path, restaurants_data, filename, extension="parquet")
