17506bdfcea739ed92a6a3efb61e4b6e4c1bed03
KristiyanBulga <56537891+KristiyanBulga@users.noreply.github.com>
2023-08-20 10:34:58 +0200

    Solving API errors

--- data_processing/functions/api.py
+++ data_processing/functions/api.py
@@ -8,19 +8,23 @@ logging.getLogger().setLevel(logging.INFO)
 def router(event, context):
     logging.info(f"Event: {event}")
     s3 = boto3.client('s3')
-    # data = s3.get_object(Bucket="trip-advisor-dev", Key="weekly_query/g187486/2023/27/g187486_2023_07_09_07_00_00.json")
-    # contents = data['Body'].read().decode("utf-8")
-    # weekly_data = json.loads(contents)
-    url = s3.generate_presigned_url(
-        ClientMethod='get_object',
-        Params={
-            'Bucket': 'trip-advisor-dev',
-            'Key': 'weekly_query/g187486/2023/27/g187486_2023_07_09_07_00_00.json'
-        },
-        ExpiresIn=3600  # one hour in seconds, increase if needed
-    )
+    data = s3.get_object(Bucket="trip-advisor-dev", Key="weekly_query/g187486/2023/27/g187486_2023_07_09_07_00_00.json")
+    contents = data['Body'].read().decode("utf-8")
+    weekly_data = json.loads(contents)
+    # url = s3.generate_presigned_url(
+    #     ClientMethod='get_object',
+    #     Params={
+    #         'Bucket': 'trip-advisor-dev',
+    #         'Key': 'weekly_query/g187486/2023/27/g187486_2023_07_09_07_00_00.json'
+    #     },
+    #     ExpiresIn=3600  # one hour in seconds, increase if needed
+    # )
     return {
         "statusCode": 200,
-        "body": json.dumps({"url": url})
-        # "body": json.dumps(weekly_data)
+        "headers": {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': True,
+        },
+        # "body": json.dumps({"url": url})
+        "body": json.dumps(weekly_data)
     }
